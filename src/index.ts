eaf28636e76fa19cdf19428680b0f4b8d5def974
majorek31 <m4y0r.offical@gmail.com>
2022-12-26 00:01:58 +0100

    review: Add mising semicolons

--- src/index.ts
+++ src/index.ts
@@ -69,13 +69,13 @@ export class Rcon {
     send(cmd: string) {
         return new Promise((resolve, reject) => {
             if (!this.authed || !this.connected)
-                reject(new Error('Authentication error'))
+                reject(new Error('Authentication error'));
             this.sendRaw(cmd, 2).then(p => resolve(p));
         })
     }
     disconnect() {
-        this.connected = false
-        this.authed = false
-        this.socket.end()
+        this.connected = false;
+        this.authed = false;
+        this.socket.end();
     }
 }
