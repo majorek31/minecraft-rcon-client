5714f275382a69830086f7d1801f3f2ff74228a1
majorek31 <m4y0r.offical@gmail.com>
2022-12-25 23:22:35 +0100

    Rework connect function

--- src/index.ts
+++ src/index.ts
@@ -22,33 +22,32 @@ export class Rcon {
     authed: boolean;
     id: number;
     constructor(options: Options){
-        this.options = options
-
-        this.connected = false
-        this.authed = false
+        this.options = options;
+        this.connected = false;
+        this.authed = false;
         this.id = 0;
     }
     connect(){
-        return new Promise<Error | null>((resolve, reject) => {
+        return new Promise((resolve, reject) => {
             this.socket = net.createConnection(this.options.port, this.options.host);
-            this.socket.on('error', () => reject(new Error('Connection error')))
-            this.socket.on('connect', () => {
-                this.connected = true
-                this.id = crypto.randomInt(32752) // random number because why not?
-                this.sendRaw(this.options.password, 3) // request id 3 stands for login request
+            this.socket.once('error', () => reject(new Error('Connection error')));
+            this.socket.once('connect', () => {
+                this.connected = true;
+                this.id = crypto.randomInt(Number.MAX_SAFE_INTEGER);
+                this.sendRaw(this.options.password, 3);
                 this.socket.once('data', (data) =>{
-                    let response = data.readInt32LE(4)
+                    let response: number = data.readInt32LE(4);
                     if (response == this.id){
-                        this.authed = true
+                        this.authed = true;
                         resolve(null);
                     }
                     else{
-                        this.disconnect()
-                        reject(new Error('Authentication error'))
+                        this.disconnect();
+                        reject(new Error('Authentication error'));
                     }
-                })
+                });
             })
-        })
+        });
     }
     sendRaw(data, requestId) {
         return new Promise((resolve, reject) => {
