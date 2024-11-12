9796a28d1dc6fb9ad4d4183920c39134cdb207a7
majorek31 <m4y0r.offical@gmail.com>
2024-11-12 22:20:24 +0100

    test: change testing password and use async/await approach

--- __tests__/checkConnect.spec.ts
+++ __tests__/checkConnect.spec.ts
@@ -1,28 +1,23 @@
 import { Rcon } from "../src/index";
-test("checkConnect", () => {
+test("checkConnect", async () => {
     const client = new Rcon({
         host: '127.0.0.1',
         port: 25575,
-        password: 'password'
+        password: 'test'
     });
-    client.connect().then(() => {
-        expect(client.isConnected()).toBe(true);
-    }).finally(() => { 
-        client.disconnect();
-    });
+    await client.connect();
+    expect(client.isConnected()).toBe(true);
+    await client.disconnect();
 });
 
-test("queryServer", () => {
+test("queryServer", async () => {
     const client = new Rcon({
         host: '127.0.0.1',
         port: 25575,
-        password: 'password'
+        password: 'test'
     });
-    client.connect().then(() => {
-        client.query('list').then((response) => {
-            expect(response).toContain('players online');
-        });     
-    }).finally(() => { 
-        client.disconnect();
-    });
+    await client.connect();
+    const response = await client.query('list');
+    expect(response).toContain('players online');
+    await client.disconnect();
 });
